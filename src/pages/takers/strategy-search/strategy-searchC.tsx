87787b2c803c634c547cc3b91fec9e5faf031528
DosImpact <ypd03008@gmail.com>
2021-07-12 18:16:35 +0900

    (docs)애러핸들링코드정리

--- src/pages/takers/strategy-search/strategy-searchC.tsx
+++ src/pages/takers/strategy-search/strategy-searchC.tsx
@@ -1,13 +1,5 @@
 import React from "react";
-import { useRecoilState } from "recoil";
-import { atomErrorStatus } from "recoil/error-state";
 const StrategySearchC = () => {
-  // const [errorStatus, setErrorStatus] = useRecoilState(atomErrorStatus);
-  // setErrorStatus((p) => ({
-  //   isError: true,
-  //   errorCode: "404",
-  // }));
-
   return <div>StrategySearch!</div>;
 };
 
