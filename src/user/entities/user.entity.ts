d34cc2f0d8d8b6cb7738c1a2563318a8a2c0afa3
DosImpact <ypd03008@gmail.com>
2021-07-20 16:15:33 +0900

    (feat)user.entity.ts

---
+++ src/user/entities/user.entity.ts
@@ -0,0 +1,50 @@
+import { IsNumber, IsString } from 'class-validator';
+import {
+  BeforeInsert,
+  BeforeUpdate,
+  Column,
+  Entity,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import * as bcrypt from 'bcrypt';
+import { InternalServerErrorException } from '@nestjs/common';
+
+@Entity()
+export class USER {
+  @IsNumber()
+  @PrimaryGeneratedColumn()
+  ID: number;
+
+  @IsString()
+  @Column()
+  EMAIL: string;
+
+  @IsString()
+  @Column()
+  NAME: string;
+
+  @IsString()
+  @Column()
+  PASSWORD: string;
+
+  @BeforeUpdate()
+  @BeforeInsert()
+  async hashPassword() {
+    if (this.PASSWORD) {
+      try {
+        this.PASSWORD = await bcrypt.hash(this.PASSWORD, 10);
+      } catch (error) {
+        throw new InternalServerErrorException();
+      }
+    }
+  }
+
+  async checkPassword(aPassword: string) {
+    try {
+      const ok = await bcrypt.compare(aPassword, this.PASSWORD);
+      return ok;
+    } catch (error) {
+      throw new InternalServerErrorException();
+    }
+  }
+}
