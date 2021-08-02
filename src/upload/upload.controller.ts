dc3fd6ab8da18e4f00fc08f9ecb2d25b1664d77b
DosImpact <ypd03008@gmail.com>
2021-08-02 12:04:19 +0900

    (feat) UploadController

--- src/upload/upload.controller.ts
+++ src/upload/upload.controller.ts
@@ -5,16 +5,31 @@ import {
   UseInterceptors,
 } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
+import { Roles } from 'src/auth/auth.decorator';
 import { UploadService } from './upload.service';
 
 @Controller('/api/upload')
 export class UploadController {
   constructor(private readonly uploadService: UploadService) {}
+  // (1) common 폴더 업로드
+  @Roles(['Any'])
   @Post('')
   @UseInterceptors(FileInterceptor('file'))
   async uploadFile(@UploadedFile() file: Express.Multer.File) {
-    // await this.uploadService.uploadS3Banner(file);
-    // await this.uploadService.uploadS3Icon(file);
     return this.uploadService.uploadS3(file);
+  }
+  // (2) icon 폴더 업로드
+  @Roles(['Any'])
+  @Post('banner')
+  @UseInterceptors(FileInterceptor('file'))
+  async uploadFileBanner(@UploadedFile() file: Express.Multer.File) {
+    return this.uploadService.uploadS3Banner(file);
+  }
+  // (3) 배너 폴더 업로드
+  @Roles(['Any'])
+  @Post('icon')
+  @UseInterceptors(FileInterceptor('file'))
+  async uploadFileIcon(@UploadedFile() file: Express.Multer.File) {
+    return this.uploadService.uploadS3Icon(file);
   }
 }
