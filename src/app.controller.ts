cb3c9b7642f5ac62a5bb3b09d42a971fa3fa8f15
DosImpact <ypd03008@gmail.com>
2021-08-03 12:24:09 +0900

    (chore) remove cahce pratice logic

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,93 +1,13 @@
-import {
-  Body,
-  CacheInterceptor,
-  CacheKey,
-  CacheTTL,
-  CACHE_MANAGER,
-  Controller,
-  Get,
-  Inject,
-  Post,
-  Query,
-  Req,
-  UseInterceptors,
-  Options,
-  HttpException,
-  HttpStatus,
-} from '@nestjs/common';
-import { Cache } from 'cache-manager';
-import { Request } from 'express';
-import { HttpCacheInterceptor } from './common/service/HttpCacheInterceptor';
+import { Controller, Get, HttpException, HttpStatus } from '@nestjs/common';
 
-@UseInterceptors(HttpCacheInterceptor)
 @Controller()
 export class AppController {
-  constructor(
-    @Inject(CACHE_MANAGER)
-    private readonly cacheManager: Cache,
-  ) {
-    const test = async () => {
-      // await cacheManager.set('name', 'dodo', { ttl: 1000 });
-      console.log(await cacheManager.get('name'));
-      console.log(await cacheManager.del('name'));
-      console.log(await cacheManager.get('name'));
-    };
-    // test();
-  }
+  constructor() {}
 
   @Get()
   getHello(): string {
-    throw new HttpException('banana BAD_REQUEST', HttpStatus.BAD_REQUEST);
-    throw new HttpException('banana Forbidden', HttpStatus.FORBIDDEN);
-    return 'Hello World!~~';
-  }
-
-  @CacheKey('adder')
-  @CacheTTL(3)
-  @Get('/adder')
-  async findAll(@Query('a') a: number, @Query('b') b: number): Promise<number> {
-    const sleep = async (ms) => new Promise((res) => setTimeout(res, ms));
-    console.log('wait...', a, '+', b);
-    await sleep(3000);
-    console.log('wait...', a, '+', b);
-    return a + b;
-  }
-
-  @Get('/adder2')
-  async findAll2(
-    @Query('a') a: number,
-    @Query('b') b: number,
-    @Req() request: Request,
-  ): Promise<number> {
-    console.log('hostname', request.hostname); //localhost
-    console.log('path', request.path); // /adder2
-    console.log('url', request.url); // /adder2?a=3&b=11
-    console.log('originalUrl', request.originalUrl); // /adder2?a=3&b=11
-    console.log(
-      'fullUrl',
-      request.protocol + '://' + request.hostname + request.originalUrl,
-    );
-
-    const memo: string | null = await this.cacheManager.get(request.url);
-    if (memo) {
-      return Number(memo);
-    }
-    const sleep = async (ms) => new Promise((res) => setTimeout(res, ms));
-    console.log('wait...', a, '+', b);
-    await sleep(3000);
-    console.log('wait...', a, '+', b);
-    const res = a + b;
-    await this.cacheManager.set(request.url, res);
-    return res;
-  }
-
-  @Post('/adder3')
-  async findAll3(@Body() { a, b }: { a: number; b: number }): Promise<number> {
-    const sleep = async (ms) => new Promise((res) => setTimeout(res, ms));
-    console.log('wait...', a, '+', b);
-    await sleep(3000);
-    console.log('wait...', a, '+', b);
-    const res = a + b;
-    return res;
+    // throw new HttpException('banana BAD_REQUEST', HttpStatus.BAD_REQUEST);
+    // throw new HttpException('banana Forbidden', HttpStatus.FORBIDDEN);
+    return '✔ Server is running';
   }
 }
