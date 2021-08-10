e2ea05e87577870b9845b1ce81a1f33b96c867a0
DosImpact <ypd03008@gmail.com>
2021-08-10 21:17:59 +0900

    (bug) 반올림

--- src/components/data-display/FNumber.tsx
+++ src/components/data-display/FNumber.tsx
@@ -8,7 +8,7 @@ const FNumber: React.FC<{
 }> = ({ val, hasPercentage = false, ...props }) => {
   return (
     <FNumberS num={val} {...props}>
-      {val * 100}
+      {Math.round(val * 100)}
       {hasPercentage ? "%" : ""}
     </FNumberS>
   );
