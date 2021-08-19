1642430ac16d19b75be64f126539377ec24fd873
changyu <changyu.park1@gmail.com>
2021-08-20 00:20:52 +0900

    (fix) 오류 수정

--- openAPI/DB/connectionPool.py
+++ openAPI/DB/connectionPool.py
@@ -15,7 +15,7 @@ class databasepool(dbQuery):
             cls._instance = super().__new__(cls)  # Foo 클래스의 객체를 생성하고 Foo._instance로 바인딩
         return cls._instance                      # Foo._instance를 리턴
 
-    def __init__(self, mode):
+    def __init__(self, mode = 0):
         cls = type(self)
         if not hasattr(cls, "_init"):             # Foo 클래스 객체에 _init 속성이 없다면
             #print("__init__ is called\n")
