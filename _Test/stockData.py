33a6feb329efc294672a8b1d12538165f8e62677
박찬규 <changyu.park1@gmail.com>
2021-08-29 16:34:21 +0900

    (feat) 재무 데이터 입력 로직 제작중

--- _Test/stockData.py
+++ _Test/stockData.py
@@ -1,4 +1,5 @@
 import dart_fss as dart
+from numpy.core.numeric import full
 from _Test.opendartAPIkey import key
 
 #open dart api 문제 : 연 1회 데이터.
@@ -24,7 +25,81 @@ class openDartAPI(object):
         fs.save()
 
 
-
-class stock(object):
+from pykrx import stock
+class  financialInformationPykrx(object):
     def __init__(self) -> None:
         super().__init__()
+
+    def getDatas(self, ticker):
+        df = stock.get_market_price_change_by_ticker(fromdate="20210517", todate="20210520")
+        print(df)
+
+        df = stock.get_market_fundamental_by_ticker(date="20210520")
+        print(df)
+
+        df = stock.get_market_fundamental_by_date(fromdate="20210517", todate="20210520", ticker="005930")
+        print(df)
+
+
+import pandas as pd
+import requests
+
+class financialInformation(object):
+    def __init__(self) -> None:
+        super().__init__()
+
+    # testurl = https://comp.fnguide.com/SVO2/ASP/SVD_main.asp?pGB=1&gicode=A005930&cID=&MenuYn=Y&ReportGB=&NewMenuID=11&stkGb=&strResearchYN=
+    def saveDatas(self, ticker, name):
+        # 발생 오류
+        # page 변경 연결 불가
+        # table 변경
+        try :
+            URL = "https://comp.fnguide.com/SVO2/ASP/SVD_main.asp?pGB=1&gicode=A"+str(ticker)+"&cID=&MenuYn=Y&ReportGB=&NewMenuID=11&stkGb=&strResearchYN="
+            
+            fs_page = requests.get(URL)
+            fs_tables = pd.read_html(fs_page.text)
+            print(fs_page)
+
+            """
+            for i in range (len(fs_tables)):
+                print(">>>>>>>  table",i)
+                print(fs_tables[i])
+            """
+
+            # 일간 재무정보
+            dailyTable = self.makeDailyFinancial(fs_tables, name)
+
+            # 분기별 재무정보
+            quearterTable = self.makeQuarterFinancial(fs_tables, name)
+
+            #데이터 합치기
+            fullTable  = pd.concat([dailyTable,quearterTable])
+
+            return fullTable
+        except Exception as error:
+            print("###finance data get error \"fs_tables\" : ", error)
+            return None
+
+    def makeQuarterFinancial(self, fs_tables, name):
+        QuarterfinancialTalbe = fs_tables[12]
+        #멀티 인덱싱(사실은 멀티 컬럼) 
+        newcol = list(map(lambda x: ' '.join(x[1:]),QuarterfinancialTalbe.columns))
+        newcol[0]='구분'
+        QuarterfinancialTalbe.columns = newcol
+
+        QuarterfinancialTalbe = QuarterfinancialTalbe.set_index('구분')
+        QuarterfinancialTalbe = QuarterfinancialTalbe.iloc[:,[-4]]
+        QuarterfinancialTalbe.columns = [name]
+        QuarterfinancialTalbe = QuarterfinancialTalbe.drop(['PER'])
+
+        return QuarterfinancialTalbe
+
+    def makeDailyFinancial(self, fs_tables, name):
+        dailyfinancialTalbe = fs_tables[8]
+        dailyfinancialTalbe = dailyfinancialTalbe.set_index('구분')
+        dailyfinancialTalbe = dailyfinancialTalbe.iloc[:,[0]]
+        dailyfinancialTalbe.columns = [name]
+
+        return dailyfinancialTalbe
+
+
--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 from _Test.getImageByURL import getAllLog
 
-from _Test.stockData import stock
+from _Test.stockData import financialInformation
+from _Test.stockData import financialInformationPykrx
 from _Test.mockInvest import MockInvest
 
 
@@ -12,6 +13,15 @@ if __name__ == '__main__':
     #getAllLog()
     #test = stock()
     #test.Test()
-    test = MockInvest()
-    test.requestMockInvestStock("test")
+    #test = MockInvest()
+    #test.requestMockInvestStock("test")
 
+    #financialInformation 테스트 코드
+    if 1:
+        test = financialInformation()
+        print(test.saveDatas("005930","삼성전자"))
+
+    # pykrx 데이터 가져오기
+    if 0:
+        test = financialInformationPykrx()
+        test.getDatas("005930")
