3d2366ac61f3ab224a78f0999b490c3daeef440e
DosImpact <ypd03008@gmail.com>
2021-09-02 20:53:25 +0900

    (feat) @sentry

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,32 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { LoggingInterceptor } from './common/service/LogginInterceptor';
 
+import * as Sentry from '@sentry/node';
+import * as Tracing from '@sentry/tracing';
+
+Sentry.init({
+  dsn: 'https://aa27bb1cf4bd48adb16f3c01dfaf422a@o986270.ingest.sentry.io/5942895',
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
+
+const transaction = Sentry.startTransaction({
+  op: 'test',
+  name: 'My First Test Transaction',
+});
+
+setTimeout(() => {
+  try {
+    throw new Error('hello my Error~~');
+  } catch (e) {
+    Sentry.captureException(e);
+  } finally {
+    transaction.finish();
+  }
+}, 99);
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(
