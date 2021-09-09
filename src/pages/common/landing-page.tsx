b0c676867283b5126a7bc2e6b1a60c2f33c6c489
DosImpact <ypd03008@gmail.com>
2021-09-09 19:03:54 +0900

    (feat) Tour

--- src/pages/common/landing-page.tsx
+++ src/pages/common/landing-page.tsx
@@ -1,12 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { Button, WhiteSpace, WingBlank } from "antd-mobile";
 import useMember from "states/react-query/useMember";
+import Tour from "reactour";
 // import SampleMarker from "components/light-weight/SampleMarker";
 
 // todo : makers, takers 선택 저장하기
 const LandingPage = () => {
   const { logIn, me } = useMember();
+
+  const [isTourOpen, setIsTourOpen] = useState(false);
   return (
     <WingBlank>
       <Button
@@ -20,6 +23,27 @@ const LandingPage = () => {
         login
       </Button>
       <WhiteSpace />
+      <button
+        onClick={() => {
+          setIsTourOpen(true);
+        }}
+      >
+        TEST Tour
+      </button>
+      <Tour
+        onRequestClose={() => {
+          setIsTourOpen(false);
+        }}
+        steps={tourConfig}
+        isOpen={isTourOpen}
+        maskClassName="mask"
+        className="helper"
+        rounded={5}
+        accentColor={"#5cb7b7"}
+        // onAfterOpen={this.disableBody}
+        // onBeforeClose={this.enableBody}
+      />
+      <WhiteSpace />
       <h1 style={{ fontSize: "20px" }}>
         알고리당에 오신것을 환영합니다.
         <br />
@@ -31,13 +55,13 @@ const LandingPage = () => {
         <ul>
           <Button type="ghost">
             <Link to="takers/ticker-search">
-              <li>전략 탐색 하기</li>
+              <li data-tut="reactour__search">전략 탐색 하기</li>
             </Link>
           </Button>
           <WhiteSpace />
           <Button type="ghost">
             <Link to="makers">
-              <li>전략 생성 하기</li>
+              <li data-tut="reactour__maker">전략 생성 하기</li>
             </Link>
           </Button>
           <WhiteSpace />
@@ -46,5 +70,16 @@ const LandingPage = () => {
     </WingBlank>
   );
 };
+
+const tourConfig = [
+  {
+    selector: '[data-tut="reactour__search"]',
+    content: `Ok, let's start with the name of the Tour that is about to begin.`,
+  },
+  {
+    selector: '[data-tut="reactour__maker"]',
+    content: `And this is our cool bus...`,
+  },
+];
 
 export default LandingPage;
