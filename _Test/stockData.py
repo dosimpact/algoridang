58ccf7e2ec926b3a59632a7ddbe8edd2bc93db19
changyu <changyu.park1@gmail.com>
2021-09-14 16:27:44 +0900

    (feat) 재무정보 DB 저장

--- _Test/stockData.py
+++ _Test/stockData.py
@@ -1,70 +1,33 @@
-import dart_fss as dart
-from numpy.core.numeric import full
-from _Test.opendartAPIkey import key
+#import dart_fss as dart
+#from numpy.core.numeric import full
+#from _Test.opendartAPIkey import key
 
-#open dart api 문제 : 연 1회 데이터.
-class openDartAPI(object):
-    def __init__(self) -> None:
-        super().__init__()
-    
-    def Test(self):
-        # Open DART API KEY 설정
-        api_key= key
-        dart.set_api_key(api_key=api_key)
-
-        # DART 에 공시된 회사 리스트 불러오기
-        corp_list = dart.get_corp_list()
-
-        # 삼성전자 검색
-        samsung = corp_list.find_by_corp_name('삼성전자', exactly=True)[0]
-
-        # 2012년부터 연간 연결재무제표 불러오기
-        fs = samsung.extract_fs(bgn_de='20120101')
-        print("123123123")
-        # 재무제표 검색 결과를 엑셀파일로 저장 ( 기본저장위치: 실행폴더/fsdata )
-        fs.save()
-
-
-from pykrx import stock
-class  financialInformationPykrx(object):
-    def __init__(self) -> None:
-        super().__init__()
-
-    def getDatas(self, ticker):
-        df = stock.get_market_price_change_by_ticker(fromdate="20210517", todate="20210520")
-        print(df)
-
-        df = stock.get_market_fundamental_by_ticker(date="20210520")
-        print(df)
-
-        df = stock.get_market_fundamental_by_date(fromdate="20210517", todate="20210520", ticker="005930")
-        print(df)
-
-
+from numpy.core.numeric import NaN
 import pandas as pd
 import requests
+from openAPI.DB.connectionPool import databasepool
+from datetime import datetime, timedelta
 
 class financialInformation(object):
     def __init__(self) -> None:
         super().__init__()
+        fullTable = []
 
-    # testurl = https://comp.fnguide.com/SVO2/ASP/SVD_main.asp?pGB=1&gicode=A005930&cID=&MenuYn=Y&ReportGB=&NewMenuID=11&stkGb=&strResearchYN=
     def saveDatas(self, ticker, name):
-        # 발생 오류
-        # page 변경 연결 불가
-        # table 변경
         try :
+            
+            # DB 틀만들기
+            #날짜 티커 정보 만들기 
+            quarterDate = self.makeQuarterDate()
+            DBTable =pd.DataFrame([('날짜',quarterDate)],columns=['구분',name]) 
+            DBTable.loc[1] = ['ticker',ticker]
+            DBTable = DBTable.set_index('구분')
+
             URL = "https://comp.fnguide.com/SVO2/ASP/SVD_main.asp?pGB=1&gicode=A"+str(ticker)+"&cID=&MenuYn=Y&ReportGB=&NewMenuID=11&stkGb=&strResearchYN="
             
             fs_page = requests.get(URL)
             fs_tables = pd.read_html(fs_page.text)
             print(fs_page)
-
-            """
-            for i in range (len(fs_tables)):
-                print(">>>>>>>  table",i)
-                print(fs_tables[i])
-            """
 
             # 일간 재무정보
             dailyTable = self.makeDailyFinancial(fs_tables, name)
@@ -73,12 +36,24 @@ class financialInformation(object):
             quearterTable = self.makeQuarterFinancial(fs_tables, name)
 
             #데이터 합치기
-            fullTable  = pd.concat([dailyTable,quearterTable])
+            self.fullTable  = pd.concat([DBTable,dailyTable,quearterTable])
+            
+            #데이터 합치기
+            self.saveDB()
 
-            return fullTable
+
         except Exception as error:
             print("###finance data get error \"fs_tables\" : ", error)
             return None
+
+
+    def makeQuarterDate(self):
+        yearToday = datetime.today().strftime("%Y")
+        monthToday = datetime.today().strftime("%m")
+        intMonthToday =  int(int(monthToday) / 3) * 3
+        monthQuarter = str(intMonthToday).rjust(2,'0')
+
+        return (yearToday + "-" + monthQuarter + "-" + '01')
 
     def makeQuarterFinancial(self, fs_tables, name):
         QuarterfinancialTalbe = fs_tables[12]
@@ -102,4 +77,109 @@ class financialInformation(object):
 
         return dailyfinancialTalbe
 
+    def saveDB(self):
+        
+        db = databasepool()
+        conn = db.getConn()
 
+        for idx in range(len(self.fullTable)):
+            val = self.fullTable.iloc[idx,0]
+            if val is None or val is NaN:
+                self.fullTable.iloc[idx,0] = 0
+                print(self.fullTable)
+
+        quarterDate = str(self.fullTable.iloc[0,0]) + "T15:30:00+09:00"
+        ticker = self.fullTable.iloc[1,0]
+
+        query = "select * from financial_statement where \"finance_date\" =\'"+str(quarterDate)+"\'and \"ticker\" = \'"+str(ticker)+"\'"
+           
+        if len(db.selectData(conn,query)) == 0:
+            query = "insert into financial_statement(\
+                \"finance_date\", \"ticker\", \"market_cap\",\
+                \"revenue\", \"operating_income\", \"EPS\",\
+                \"PER\", \"EV_per_EBITDA\", \"ROE\",\
+                \"dividend_yield\", \"BETA\", \"revenue_Q\",\
+                \"operating_income_Q\", \"net_income_Q\", \"controlling_interest_Q\",\
+                \"non_controlling_interest_Q\", \"total_assets_Q\", \"total_stock_holders_Q\",\
+                \"controlling_interest_share_Q\", \"non_controlling_interest_share_Q\", \"capital_Q\",\
+                \"debt_ratio_Q\", \"retention_rate_Q\", \"operating_margin_Q\",\
+                \"controlling_interest_rate_Q\", \"ROA_Q\", \"ROE_Q\",\
+                \"EPS_Q\", \"BPS_Q\", \"DPS_Q\",\
+                \"PBR_Q\", \"outstanding_shares_Q\", \"dividend_yield__Q\") "
+
+            query += "values(\
+                \'"+str(quarterDate)+"\',\'"+str(ticker)+"\',\
+                "+str(int(self.fullTable.iloc[2,0] ))+",\
+                "+str(int(self.fullTable.iloc[3,0] ))+",\
+                "+str(int(self.fullTable.iloc[4,0] ))+",\
+                "+str(int(self.fullTable.iloc[5,0] ))+",\
+                "+str(self.fullTable.iloc[6,0] )+",\
+                "+str(self.fullTable.iloc[8,0] )+",\
+                "+str(self.fullTable.iloc[7,0] )+",\
+                "+str(self.fullTable.iloc[9,0] )+",\
+                "+str(self.fullTable.iloc[10,0])+",\
+                "+str(int(self.fullTable.iloc[11,0]))+",\
+                "+str(int(self.fullTable.iloc[12,0]))+",\
+                "+str(int(self.fullTable.iloc[13,0]))+",\
+                "+str(int(self.fullTable.iloc[14,0]))+",\
+                "+str(int(self.fullTable.iloc[15,0]))+",\
+                "+str(int(self.fullTable.iloc[16,0]))+",\
+                "+str(int(self.fullTable.iloc[17,0]))+",\
+                "+str(int(self.fullTable.iloc[18,0]))+",\
+                "+str(int(self.fullTable.iloc[19,0]))+",\
+                "+str(int(self.fullTable.iloc[20,0]))+",\
+                "+str(self.fullTable.iloc[21,0])+",\
+                "+str(self.fullTable.iloc[22,0])+",\
+                "+str(self.fullTable.iloc[23,0])+",\
+                "+str(self.fullTable.iloc[24,0])+",\
+                "+str(self.fullTable.iloc[25,0])+",\
+                "+str(self.fullTable.iloc[26,0])+",\
+                "+str(int(self.fullTable.iloc[27,0]))+",\
+                "+str(int(self.fullTable.iloc[28,0]))+",\
+                "+str(int(self.fullTable.iloc[29,0]))+",\
+                "+str(self.fullTable.iloc[30,0])+",\
+                "+str(int(self.fullTable.iloc[31,0]))+",\
+                "+str(self.fullTable.iloc[32,0])+")"
+            
+            db.insertIntoData(conn,query)
+            conn.commit()
+            
+        else:
+            query = "update financial_statement set "
+            query += " \"market_cap\" = "                       + str(int(self.fullTable.iloc[2,0]))+ ","
+            query += " \"revenue\" = "                          + str(int(self.fullTable.iloc[3,0]))+ ","
+            query += " \"operating_income\" = "                 + str(int(self.fullTable.iloc[4,0]))+ ","
+            query += " \"EPS\" = "                              + str(int(self.fullTable.iloc[5,0]))+ ","
+            query += " \"PER\" = "                              + str(self.fullTable.iloc[6,0])+ ","
+            query += " \"EV_per_EBITDA\" = "                    + str(self.fullTable.iloc[7,0])+ ","
+            query += " \"ROE\" = "                              + str(self.fullTable.iloc[8,0])+ ","
+            query += " dividend_yield = "                   + str(self.fullTable.iloc[9,0])+ ","
+            query += " \"BETA\" = "                             + str(self.fullTable.iloc[10,0])+ ","
+            query += " \"revenue_Q\" = "                        + str(int(self.fullTable.iloc[11,0]))+ ","
+            query += " \"operating_income_Q\" = "               + str(int(self.fullTable.iloc[12,0]))+ ","
+            query += " \"net_income_Q\" = "                     + str(int(self.fullTable.iloc[13,0]))+ ","
+            query += " \"controlling_interest_Q\" = "           + str(int(self.fullTable.iloc[14,0]))+ ","
+            query += " \"non_controlling_interest_Q\" = "       + str(int(self.fullTable.iloc[15,0]))+ ","
+            query += " \"total_assets_Q\" = "                   + str(int(self.fullTable.iloc[16,0]))+ ","
+            query += " \"total_stock_holders_Q\" = "            + str(int(self.fullTable.iloc[17,0]))+ ","
+            query += " \"controlling_interest_share_Q\" = "     + str(int(self.fullTable.iloc[18,0]))+ ","
+            query += " \"non_controlling_interest_share_Q\" = " + str(int(self.fullTable.iloc[19,0]))+ ","
+            query += " \"capital_Q\" = "                        + str(int(self.fullTable.iloc[20,0]))+ ","
+            query += " \"debt_ratio_Q\" = "                     + str(self.fullTable.iloc[21,0])+ ","
+            query += " \"retention_rate_Q\" = "                 + str(self.fullTable.iloc[22,0])+ ","
+            query += " \"operating_margin_Q\" = "               + str(self.fullTable.iloc[23,0])+ ","
+            query += " \"controlling_interest_rate_Q\" = "      + str(self.fullTable.iloc[24,0])+ ","
+            query += " \"ROA_Q\" = "                            + str(self.fullTable.iloc[25,0])+ ","
+            query += " \"ROE_Q\" = "                            + str(self.fullTable.iloc[26,0])+ ","
+            query += " \"EPS_Q\" = "                            + str(int(self.fullTable.iloc[27,0]))+ ","
+            query += " \"BPS_Q\" = "                            + str(int(self.fullTable.iloc[28,0]))+ ","
+            query += " \"DPS_Q\" = "                            + str(int(self.fullTable.iloc[29,0]))+ ","
+            query += " \"PBR_Q\" = "                            + str(self.fullTable.iloc[30,0])+ ","
+            query += " \"outstanding_shares_Q\" = "             + str(int(self.fullTable.iloc[31,0]))+ ","
+            query += " \"dividend_yield__Q\" = "                + str(self.fullTable.iloc[32,0])
+            query += " where \"finance_date\" = \'" + str(quarterDate) + "\'"
+            query += " and \"ticker\" = \'" + str(ticker) + "\'"
+    
+            db.updateData(conn,query)
+            conn.commit()
+        db.putConn(conn)
--- test.py
+++ test.py
@@ -1,27 +1,12 @@
-from _Test.getImageByURL import getAllLog
 
 from _Test.stockData import financialInformation
-from _Test.stockData import financialInformationPykrx
-from _Test.mockInvest import MockInvest
 
 
 
 
 
 if __name__ == '__main__':
-    #logo 가져오기
-    #getAllLog()
-    #test = stock()
-    #test.Test()
-    #test = MockInvest()
-    #test.requestMockInvestStock("test")
-
     #financialInformation 테스트 코드
     if 1:
         test = financialInformation()
         print(test.saveDatas("005930","삼성전자"))
-
-    # pykrx 데이터 가져오기
-    if 0:
-        test = financialInformationPykrx()
-        test.getDatas("005930")
