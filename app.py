9a49ff1b16f3f5c03615da507246b022db33601c
changu100 <blue_rose17@naver.com>
2021-10-19 19:39:09 +0900

    db 수정

--- app.py
+++ app.py
@@ -51,7 +51,7 @@ api.add_namespace(DailyStock, '/datas')
 api.add_namespace(Quant, '/quant')
 
 
-dbinit = databasepool(0)
+dbinit = databasepool(2)
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
     #processor.Test___backtestTestCode(2733)
